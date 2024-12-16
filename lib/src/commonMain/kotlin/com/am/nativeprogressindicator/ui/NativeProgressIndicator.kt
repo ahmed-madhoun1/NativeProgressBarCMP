81f1623f2ff5682e8d9f85d8063ce204a1952a02
Ahmed Madhoun <68607257+ahmed-madhoun1@users.noreply.github.com>
2024-12-16 19:32:22 +0200

    Create NativeProgressIndicator.kt

---
+++ lib/src/commonMain/kotlin/com/am/nativeprogressindicator/ui/NativeProgressIndicator.kt
@@ -0,0 +1,7 @@
+package com.am.nativeprogressindicator.ui
+
+import androidx.compose.runtime.Composable
+
+// Common expect function to be implemented platform-specific
+@Composable
+expect fun PlatformSpecificProgressIndicator()
